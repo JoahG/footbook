7f2eb0976331129ab61c7f91eb25276c90e60917
Charles <charlesmanfre@gmail.com>
2013-05-02 12:45:57 +1000

    Updated front-end for new authentication system (prob. too many features for 1 commit)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Footbook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Ensures defined default url options in environments files
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
